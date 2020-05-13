bf0b97693afe7cfd7ba9430e94b759542cc096ea
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-13 03:52:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -59,4 +59,12 @@ def self.new_from_filename(file)
    self.new(song_name, artist, genre)
  end
 
+ def self.create_from_filename(filename)
+   self.new_from_filename(filename).save
+ end
+
+ def save
+   @@all << self
+ end
+ 
 end
