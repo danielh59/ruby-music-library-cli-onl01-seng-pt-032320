6e86eb48e106acc52f2f3ffa8c112758d7dfa3f8
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-13 03:49:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,5 +47,16 @@ def self.find_or_create_by_name(song)
   self.find_by_name(song) || self.create(song)
 end
 
+def self.new_from_filename(file)
+   file_array= file.split(" - ")
+
+   song_name = file_array[1]
+   artist_name = file_array[0]
+   genre_name = file_array[2].split(".mp3").join
+
+   artist = Artist.find_or_create_by_name(artist_name)
+   genre = Genre.find_or_create_by_name(genre_name)
+   self.new(song_name, artist, genre)
+ end
 
 end
